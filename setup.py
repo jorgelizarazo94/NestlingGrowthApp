308a659a3c460c960d84193bff4748ec84183194
jorgelizarazo94 <126923563+jorgelizarazo94@users.noreply.github.com>
2025-03-25 20:08:35 -0500

    eed

--- setup.py
+++ setup.py
@@ -2,11 +2,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='nestling-growth-app',
+    name='nestling_growth_app',
     version='0.1.5',
-    description='Dash app for analyzing nestling growth with logistic and non-linear models.',
-    author='Jorge Lizarazo',
-    packages=find_packages(include=['api', 'models', 'components']),
+    packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'dash',
@@ -22,13 +20,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'nestling-app=api.app:main'
-        ],
+            'nestling-app = Nestling_App.api.app:main'
+        ]
     },
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'Framework :: Dash',
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.7',
 )
